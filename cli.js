88dbce0e7667a37fefcdf6cac5267a96045229d3
utk9 <utkarshlamba.2@gmail.com>
2017-10-04 18:22:46 -0700

    Add ability to create a lambda

--- cli.js
+++ cli.js
@@ -2,8 +2,24 @@
 
 'use strict';
 
-const yargsOuter = require('yargs');
 const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+
+const archiver = require('archiver');
+const yargsOuter = require('yargs');
+const promisify = require('es6-promisify');
+const shortid = require('shortid');
+const jsonfile = require('jsonfile');
+const AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./cred.json');
+AWS.config.setPromisesDependency(Promise);
+
+const lambda = new AWS.Lambda();
+
+const readFileAsync = promisify(fs.readFile);
+const readJsonFileAsync = promisify(jsonfile.readFile)
 
 const PackageSizes = {
     small: 'small',
@@ -21,7 +37,52 @@ const Actions = {
     update: 'update',
 };
 
-const runLambdaBenchmarkCliAsync = async function(): Promise<void> {
+async function createDeploymentPackageAsync(lambdaId) {
+    const outputPath = path.join(__dirname, 'lambda', 'deployment_packages', `${lambdaId}.zip`)
+    const output = fs.createWriteStream(outputPath);
+    const zip = archiver('zip');
+
+    await new Promise((resolve, reject) => {
+        output.on('close', resolve);
+        zip.on('error', reject);
+        zip.pipe(output);
+        zip.glob('*', {
+            cwd: path.join(__dirname, 'lambda', 'small'),
+        })
+        zip.finalize();
+    });
+    return outputPath;
+}
+
+async function getCreateFunctionParamsAsync(size) {
+    const lambdaId = shortid.generate();
+    const packagePath = await createDeploymentPackageAsync(lambdaId);
+    const bundleAndRole = await Promise.all([readFileAsync(packagePath), readJsonFileAsync('lambda_config.json')]);
+    const ZipFile = bundleAndRole[0];
+    const {Role} = bundleAndRole[1];
+    return {
+        Code: {ZipFile},
+        FunctionName: `test-function-${lambdaId}`,
+        Handler: "index.handler",
+        Role,
+        Runtime: "nodejs4.3",
+    };
+}
+
+function invokeLambda() {
+}
+
+async function createLambdaAsync(size) {
+    const params = await getCreateFunctionParamsAsync(size);
+    return lambda.createFunction(params).promise();
+}
+
+function _exitWithError(e) {
+    console.log(e.message, e.stack);
+    process.exit(1);
+}
+
+async function runLambdaBenchmarkCliAsync() {
     const config = yargsOuter
         .usage('Usage: $0 <command> [options] ')
         .option('size', {
@@ -38,14 +99,31 @@ const runLambdaBenchmarkCliAsync = async function(): Promise<void> {
         })
         .help('help')
         .check(yargs => {
-            const {size, hosted, action} = yargs;
-            if (!size || !hosted || !action) {
-                throw h.utils.spawnError('Missing options');
-            }
+            // const {size, hosted, action} = yargs;
+            // if (!size || !hosted || !action) {
+            //     throw new Error('Missing options');
+            // }
             return true;
         })
         .argv;
-    // perform correct action based on options
+
+    const {action} = config;
+    if (action === Actions.create) {
+        const start = new Date();
+        try {
+            await createLambdaAsync();
+            const end = new Date();
+            console.log(`Time taken to create lambda: ${end-start}`);
+        } catch (e) {
+            _exitWithError(e);
+        }
+    } else if (action === Actions.update) {
+        // TODO: implement this
+        _exitWithError(new Error("NOT IMPLEMENTED YET!"));
+    } else {
+        yargsOuter.showHelp();
+        _exitWithError(new Error("Invalid action"));
+    }
 };
 
-h.utils.fireAndForgetPromise(runBlocksCliAsync)();
+runLambdaBenchmarkCliAsync();
