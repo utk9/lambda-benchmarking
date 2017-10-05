9a6c4913d3b692124fd1ead05ba754d54169c648
utk9 <utkarshlamba.2@gmail.com>
2017-10-05 16:44:17 -0700

    Use timer to get more granular timing

--- cli.js
+++ cli.js
@@ -14,6 +14,8 @@ const promisify = require('es6-promisify');
 const shortid = require('shortid');
 const yargsOuter = require('yargs');
 
+const {Timer} = require('./timer');
+
 
 AWS.config.loadFromPath('./cred.json');
 AWS.config.setPromisesDependency(Promise);
@@ -42,7 +44,7 @@ const Actions = {
     update: 'update',
 };
 
-async function createDeploymentPackageAsync(lambdaId, size = PackageSizes.medium) {
+async function _createDeploymentPackageAsync(lambdaId, size = PackageSizes.medium) {
     const outputPath = path.join(__dirname, 'lambda', 'deployment_packages', `${lambdaId}.zip`)
     const output = fs.createWriteStream(outputPath);
     const zip = archiver('zip');
@@ -59,9 +61,7 @@ async function createDeploymentPackageAsync(lambdaId, size = PackageSizes.medium
     return outputPath;
 }
 
-async function _uploadPackageToS3Async(lambdaId, size) {
-    const packagePath = await createDeploymentPackageAsync(lambdaId, size);
-    const bundle = await readFileAsync(packagePath);
+async function _uploadPackageToS3Async(bundle, size) {
     const params = {
         Bucket: S3_BUCKET,
         Key: `${lambdaId}Bundle`,
@@ -82,21 +82,32 @@ async function _createLambdaAsync(viaS3 = false, size) {
         Role,
         Runtime: 'nodejs4.3',
     }
+
+    const timer = new Timer();
+
+    const packagePath = await _createDeploymentPackageAsync(lambdaId, size);
+    timer.addCheckpointAndLog('Created deployment package');
+
+    const bundle = await readFileAsync(packagePath);
+    timer.addCheckpointAndLog('Read bundle from file system');
+
     if (viaS3) {
-        const {Bucket, Key} = await _uploadPackageToS3Async(lambdaId, size);
+        const {Bucket, Key} = await _uploadPackageToS3Async(bundle, size);
+        timer.addCheckpointAndLog('Uploaded package to S3');
         params.Code = {
             S3Bucket: Bucket,
             S3Key: Key,
         }
     } else {
-        const packagePath = await createDeploymentPackageAsync(lambdaId, size);
-        const bundle = await readFileAsync(packagePath);
         params.Code = {
             ZipFile: bundle,
         }
     }
     // upload to lambda
-    return await lambda.createFunction(params).promise();
+    const result = await lambda.createFunction(params).promise();
+    timer.addCheckpointAndLog(`Created Function: ${result.FunctionName}`);
+    timer.stop();
+    return result;
 }
 
 async function _getExisingLambdaAsync() {
@@ -115,17 +126,30 @@ async function _updateLambdaAsync(lambdaToUpdate, viaS3 = false, size) {
     const params = {
         FunctionName: lambdaId,
     }
+
+    const timer = new Timer();
+
+    const packagePath = await _createDeploymentPackageAsync(lambdaId, size);
+    timer.addCheckpointAndLog('Created deployment package');
+
+    const bundle = await readFileAsync(packagePath);
+    timer.addCheckpointAndLog('Read bundle from file system');
+
     if (viaS3) {
-        const {Bucket, Key} = await _uploadPackageToS3Async(lambdaId, size);
+        const {Bucket, Key} = await _uploadPackageToS3Async(bundle, size);
+        timer.addCheckpointAndLog('Uploaded package to S3');
         params.S3Bucket = Bucket;
         params.S3Key = Key;
     } else {
-        const packagePath = await createDeploymentPackageAsync(lambdaId, size);
+        const packagePath = await _createDeploymentPackageAsync(lambdaId, size);
         const bundle = await readFileAsync(packagePath);
         params.ZipFile = bundle;
     }
     // update lambda code
-    return await lambda.updateFunctionCode(params).promise();
+    const result = await lambda.updateFunctionCode(params).promise();
+    timer.addCheckpointAndLog(`Updated Function: ${result.FunctionName}`);
+    timer.stop();
+    return result;
 }
 
 async function _invokeLambdaAsync(lambdaId) {
@@ -142,22 +166,22 @@ function _exitWithError(e) {
     process.exit(1);
 }
 
-async function _timeFunctionExecution(func, ...args) {
-    const hrstart = process.hrtime();
+async function _timeFunctionExecution(message = 'Total execution time', func, ...args) {
+    const timer = new Timer();
     const result = await func(...args);
-    return {
-        result,
-        timeTaken: process.hrtime(hrstart),
-    }
+    timer.addCheckpointAndLog(message);
+    timer.stop();
+    return result;
 }
 
 async function _invokeLambdaInSeriesAsync(lambdaId, numInvocations) {
     const range = _.range(numInvocations);
+    const timer = new Timer();
     for (const i of range) {
-        const {result: invocationResult, timeTaken} = await _timeFunctionExecution(_invokeLambdaAsync, lambdaId);
-        console.log(`Invocation ${i+1} complete:`, invocationResult);
-        console.info("Execution time: %ds %dms".underline.green, timeTaken[0], timeTaken[1]/1000000);
+        await _invokeLambdaAsync(lambdaId);
+        timer.addCheckpointAndLog(`Invocation ${i}`);
     }
+    timer.stop();
 }
 
 async function runLambdaBenchmarkCliAsync() {
@@ -197,9 +221,7 @@ async function runLambdaBenchmarkCliAsync() {
     const {action, hosted, size, invoke, numInvocations} = config;
     if (action === Actions.create) {
         try {
-            const {result, timeTaken} = await _timeFunctionExecution(_createLambdaAsync, hosted === HostedOptions.s3, size);
-            console.log(`Lambda created: ${result.FunctionName}`);
-            console.info("Execution time: %ds %dms".underline.green, timeTaken[0], timeTaken[1]/1000000);
+            const result = await _timeFunctionExecution('Total create time', _createLambdaAsync, hosted === HostedOptions.s3, size);
             if (invoke) {
                 _invokeLambdaInSeriesAsync(result.FunctionName, numInvocations);
             }
@@ -209,9 +231,7 @@ async function runLambdaBenchmarkCliAsync() {
     } else if (action === Actions.update) {
         try {
             const lambdaToUpdate = await _getExisingLambdaAsync();
-            const {result, timeTaken} = await _timeFunctionExecution(_updateLambdaAsync, lambdaToUpdate, hosted === HostedOptions.s3, size)
-            console.log(`Lambda updated: ${result.FunctionName}`);
-            console.info("Execution time: %ds %dms".underline.green, timeTaken[0], timeTaken[1]/1000000);
+            const result = await _timeFunctionExecution('Total update time', _updateLambdaAsync, lambdaToUpdate, hosted === HostedOptions.s3, size)
             if (invoke) {
                 _invokeLambdaInSeriesAsync(result.FunctionName, numInvocations);
             }
